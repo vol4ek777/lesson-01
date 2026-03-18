2ab7a19dd4e4ca43b4d9e9963ae9ec5aed38431a
vol4ek777 <ximaxi1995@gmail.com>
2026-03-18 18:28:49 +0200

    lesson2done

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -16,3 +16,10 @@ const hasTemporaryPass = false
 let isAccess
 
 // your code
+if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+  isAccess = true
+} else {
+  isAccess = false
+}
+
+console.log(isAccess) 
