14df6779d10781bddf279f91272d6818c003c5cc
vol4ek777 <ximaxi1995@gmail.com>
2026-03-18 18:40:01 +0200

    lesson2done

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,3 +7,17 @@
 const score = 70 // тестовое значение, можно изменять
 let grade
 // your code
+if (score >= 90 && score <= 100) {
+  grade = 'A'
+} else if (score >= 80 && score <= 89) {
+  grade = 'B'
+} else if (score >= 70 && score <= 79) {
+  grade = 'C'
+} else if (score >= 50 && score <= 69) {
+  grade = 'D'
+} else if (score >= 0 && score <= 49) {
+  grade = 'F'
+} else {
+  grade = 'small score'
+}
+
